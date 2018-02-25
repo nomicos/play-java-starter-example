03e9ffd81c3faf27da68c63e23fac08c7749d125
Andrii Bida <ab.nomicos@gmail.com>
2018-02-25 22:31:21 +0100

    Create BooksController

---
+++ app/controllers/BooksController.java
@@ -0,0 +1,38 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class BooksController extends Controller {
+
+    // Show all books.
+    public Result index() {
+        return TODO;
+    }
+
+    // Show book details.
+    public Result show(Integer id) {
+        return TODO;
+    }
+
+    public Result create() {
+        return TODO;
+    }
+
+    public Result save() {
+        return TODO;
+    }
+
+    public Result edit(Integer id) {
+        return TODO;
+    }
+
+    public Result update() {
+        return TODO;
+    }
+
+    public Result destroy(Integer id) {
+        return TODO;
+    }
+
+}
