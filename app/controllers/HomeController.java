6bc413c1e49398e45f38519e94aa9395440ae1ed
Andrii Bida <ab.nomicos@gmail.com>
2018-02-25 22:20:07 +0100

    Add dummy variables to HomeController.index (debugging fun)

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -18,7 +18,19 @@ public class HomeController extends Controller {
      * <code>GET</code> request with a path of <code>/</code>.
      */
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+
+        // These variables can be viewed through debugger. Run the app:
+        // `sbt run -jvm-debug 9999`
+        // and create a run configuration in IntelliJ:
+        // Run > Edit Configurations > [plus] > Remote, port 9999
+        // then set breakpoints, run debugging and access the app through the browser.
+        //
+        // https://youtu.be/RVKU9JvZmao - Debug Play application in IntelliJ
+
+        int x = 100;
+        String s = "lorem ipsum dolor";
+
+        return ok(index.render(s + " / Text from HomeController.index()."));
     }
 
     public Result welcome(String name, String lastName) {
