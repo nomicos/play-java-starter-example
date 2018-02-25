b8ab6582484f716c066e4acc1200c81a0538638d
Andrii Bida <ab.nomicos@gmail.com>
2018-02-25 22:02:11 +0100

    Add a route with variables for HomeController.welcome

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -20,4 +20,8 @@ public class HomeController extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
+    public Result welcome(String name, String lastName) {
+        return ok("Welcome, " + name + " " + lastName + "!");
+    }
+
 }
