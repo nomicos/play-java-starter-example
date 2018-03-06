189f733634006ad469e00c6564990910333e006c
Andrii Bida <ab.nomicos@gmail.com>
2018-03-06 23:40:25 +0100

    Implement BooksController.edit and a view for it

--- app/controllers/BooksController.java
+++ app/controllers/BooksController.java
@@ -39,7 +39,12 @@ public class BooksController extends Controller {
     }
 
     public Result edit(Integer id) {
-        return TODO;
+        Book book = Book.findById(id);
+        if (book == null) {
+            return notFound("Book not found");
+        }
+        Form<Book> bookForm = formFactory.form(Book.class).fill(book);
+        return ok(edit.render(bookForm));
     }
 
     public Result update() {
